6136e3aebce94bb2d2854688feddb3fa03546c78
daxas-boop <daxas.swd@hotmail.com>
2020-09-04 00:38:45 -0300

    Now you can switch tabs.

--- src/index.js
+++ src/index.js
@@ -1,39 +1,49 @@
 import renderHomePage from './home.js';
 import {renderHeader, renderFooter} from './header-footer.js';
-import {renderAbout as about} from './about.js';
+import {renderAbout as $about} from './about.js';
+import {renderMenu as $menu} from './menu.js';
+
 
 const $container = document.querySelector('#content');
 
 function initialRender(){
+    $container.innerHTML = '';
     const $header = renderHeader();
     const $footer = renderFooter();
     const $mainElement = renderHomePage();
     $container.appendChild($header);
     $container.appendChild($mainElement);
     $container.appendChild($footer);
+    handleTabSwitch()
 }
 
 function renderAbout() {
     $container.innerHTML = '';
     const $header = renderHeader();
     const $footer = renderFooter();
-    const $mainElement = about();
+    const $mainElement = $about();
+    $container.appendChild($header);
+    $container.appendChild($mainElement);
+    $container.appendChild($footer);
+    
+}
+
+function renderMenu() {
+    $container.innerHTML = '';
+    const $header = renderHeader();
+    const $footer = renderFooter();
+    const $mainElement = $menu();
     $container.appendChild($header);
     $container.appendChild($mainElement);
     $container.appendChild($footer);
 }
 
-
-function handleTabSwitch() { // need to call handleTab every time a tab is switched
-    const $home = document.querySelector('#home');
-    const $menu = document.querySelector('#menu');
-    const $contact = document.querySelector('#contact');
-    const $about = document.querySelector('#about');
-
-    $home.addEventListener('click', e =>{initialRender(), addActiveClass(e)})
-    $menu.addEventListener('click', e =>{addActiveClass(e)})
-    $contact.addEventListener('click', e =>{addActiveClass(e)})
-    $about.addEventListener('click', e => {renderAbout(), addActiveClass(e)})
+function renderContact() {
+    $container.innerHTML = '';
+    const $header = renderHeader();
+    const $footer = renderFooter();
+    $container.appendChild($header);
+    $container.appendChild($footer);
 }
 
 function addActiveClass(e) {
@@ -43,6 +53,16 @@ function addActiveClass(e) {
     $current.classList.add('active');
 }
 
+function handleTabSwitch() {
+    const $home = document.querySelector('#home');
+    const $menu = document.querySelector('#menu');
+    const $contact = document.querySelector('#contact');
+    const $about = document.querySelector('#about');
+
+    $home.addEventListener('click', e =>{initialRender(), addActiveClass(e), handleTabSwitch();})
+    $menu.addEventListener('click', e =>{renderMenu(), addActiveClass(e), handleTabSwitch();})
+    $contact.addEventListener('click', e =>{renderContact(), addActiveClass(e), handleTabSwitch();})
+    $about.addEventListener('click', e => {renderAbout(), addActiveClass(e), handleTabSwitch();})
+}
 
 initialRender();
-handleTabSwitch();
