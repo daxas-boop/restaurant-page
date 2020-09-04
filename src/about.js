1a71dd5d783a376ff4b5dc6739fa07bff687c6d5
daxas-boop <daxas.swd@hotmail.com>
2020-09-04 00:39:37 -0300

    Finishes the about.js module

--- src/about.js
+++ src/about.js
@@ -2,10 +2,16 @@ export function renderAbout() {
     let $about = document.createElement('div');
     $about.setAttribute('id', 'main-content-about');
     let $textContainer = document.createElement('div');
-    $textContainer.setAttribute('id', 'text-container');
+    $textContainer.setAttribute('id', 'text-container-about');
     let $aboutText = document.createElement('p');
     $aboutText.innerText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam a quas aliquid pariatur consequatur veritatis libero ut! Illo, harum explicabo iste at culpa fugit numquam perferendis deleniti, ad doloribus delectus.'
+    let $imageContainer = document.createElement('div');
+    $imageContainer.setAttribute('id', 'image-container-about');
+    let $aboutImage = document.createElement('img');
+    $aboutImage.setAttribute('src', './images/About-us.png');
+    $imageContainer.appendChild($aboutImage);
     $textContainer.appendChild($aboutText);
+    $about.appendChild($imageContainer);
     $about.appendChild($textContainer);
     
     return $about;
