c7f024cbc8db4dd3e7f71850ff50cc73f6c63ce1
daxas-boop <daxas.swd@hotmail.com>
2020-09-06 22:18:04 -0300

    Refactors code. Easier to understand and mantain

--- src/contact.js
+++ src/contact.js
@@ -9,41 +9,23 @@ export function renderContact() {
         $formTitle.innerText = 'Envienos un mensaje.';
         let $form = document.createElement('form');
         $form.setAttribute('id', 'form');
-            
-        let $labelName = document.createElement('label');
-        $labelName.setAttribute('for', 'name');
-        $labelName.innerText = 'Nombre*';
-        let $inputName = document.createElement('input');
-        $inputName.setAttribute('name', 'name');
-        $inputName.setAttribute('type', 'text');
-        $labelName.appendChild($inputName);
-        $form.appendChild($labelName);
 
-        let $labelEmail = document.createElement('label');
-        $labelEmail.setAttribute('for', 'email');
-        $labelEmail.innerText = 'E-mail:';
-        let $inputEmail = document.createElement('input');
-        $inputEmail.setAttribute('name', 'email');
-        $inputEmail.setAttribute('type', 'text');
-        $labelEmail.appendChild($inputEmail);
-        $form.appendChild($labelEmail);
+        let inputs = [{for:'name', text:'Nombre*', type:'text'}, 
+        {for:'email', text:'E-mail:',type:'text'}, 
+        {for:'phone', text:'Telefono:', type:'text'},
+        {for:'comment', text:'Su mensaje', type:''}];
 
-        let $labelPhone = document.createElement('label');
-        $labelPhone.setAttribute('for', 'phone');
-        $labelPhone.innerText = 'Telefono:';
-        let $inputPhone = document.createElement('input');
-        $inputPhone.setAttribute('name', 'phone');
-        $inputPhone.setAttribute('type', 'text');
-        $labelPhone.appendChild($inputPhone);
-        $form.appendChild($labelPhone);
-
-        let $labelComment = document.createElement('label');
-        $labelComment.setAttribute('for', 'comment');
-        $labelComment.innerText = 'Su mensaje*';
-        let $inputComment = document.createElement('textarea');
-        $inputComment.setAttribute('name', 'comment');
-        $labelComment.appendChild($inputComment);
-        $form.appendChild($labelComment);
+        inputs.forEach( input => {
+            let $label = document.createElement('label');
+            $label.setAttribute('for', `${input.for}`);
+            $label.innerText = `${input.text}`;
+            let $input = document.createElement('input');
+            if(input.for === 'comment') { $input = document.createElement('textarea') }
+            $input.setAttribute('name', `${input.for}`);
+            $input.setAttribute('type', `${input.type}`);
+            $label.appendChild($input);
+            $form.appendChild($label);
+        });
 
         let $button = document.createElement('button');
         $button.setAttribute('type', 'reset');
@@ -57,39 +39,22 @@ export function renderContact() {
     (function renderSocialInfoBox() {
         let $socialInfoContainer = document.createElement('div');
         $socialInfoContainer.setAttribute('id', 'social-info-container');
-        
-        let $phoneInfo = document.createElement('div');
-        $phoneInfo.setAttribute('id', 'phone-info');
-        let $phoneInfoTitle = document.createElement('h2');
-        $phoneInfoTitle.innerText = 'Telefono:';
-        let $phoneInfoText = document.createElement('p');
-        $phoneInfoText.innerText = '15-44443333 / 4222-3344';
-        $phoneInfo.appendChild($phoneInfoTitle);
-        $phoneInfo.appendChild($phoneInfoText);
-        $socialInfoContainer.appendChild($phoneInfo);
-        
-        let $emailInfo = document.createElement('div');
-        $emailInfo.setAttribute('id', 'email-info');
-        let $emailInfoTitle = document.createElement('h2');
-        $emailInfoTitle.innerText = 'Email:';
-        let $emailInfoText = document.createElement('p');
-        $emailInfoText.innerText = 'email@gmail.com';
-        $emailInfo.appendChild($emailInfoTitle);
-        $emailInfo.appendChild($emailInfoText);
-        $socialInfoContainer.appendChild($emailInfo);
-            
-        let $whereInfo = document.createElement('div');
-        $whereInfo.setAttribute('id', 'where-info');
-        let $whereInfoTitle = document.createElement('h2');
-        $whereInfoTitle.innerText = 'Estamos en:';
-        let $whereInfoText = document.createElement('p');
-        $whereInfoText.innerText = 'Pl. Sant Jaume, 3241';
-        let $whereInfoText1 = document.createElement('p');
-        $whereInfoText1.innerText = 'Buenos Aires, Argentina';
-        $whereInfo.appendChild($whereInfoTitle);
-        $whereInfo.appendChild($whereInfoText);
-        $whereInfo.appendChild($whereInfoText1);
-        $socialInfoContainer.appendChild($whereInfo);
+
+        let infoBoxes = [{id:'phone-info', title:'Telefono:', text:'15-44443333 / 4222-3344'},
+        {id:'email-info', title:'Email:', text:'email@gmail.com'},
+        {id:'where-info', title:'Estamos en:', text:'Pl. Sant Jaume, 3241. Buenos Aires, Argentina'},];
+
+        infoBoxes.forEach( infoBox => {
+            let $infoBox = document.createElement('div');
+            $infoBox.setAttribute('id', `${infoBox.id}`);
+            let $title = document.createElement('h2');
+            $title.innerText = `${infoBox.title}`;
+            let $text = document.createElement('p');
+            $text.innerText = `${infoBox.text}`;
+            $infoBox.appendChild($title);
+            $infoBox.appendChild($text);
+            $socialInfoContainer.appendChild($infoBox);
+        });
         
         $contact.appendChild($socialInfoContainer);
     }());
